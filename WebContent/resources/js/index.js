55da14695a72e4e03dff4d1524b12ca1b8380139
jinzhk <jinzhk@inspur.com>
2015-06-09 15:29:29 +0800

    完善了ajax错误信息提示

--- WebContent/resources/js/index.js
+++ WebContent/resources/js/index.js
@@ -1,43 +1,50 @@
 define(['Common'],function(Common){
 	var init = function(){
-		Common.render(true, 'tpls/index.html', {
-			iSelectData: {
-				name: 'xxxx',
-				attrs: {
-					'data-xxx': 'xxx'
-				},
-				list: [
-					{
-						value: 1245,
-						attrs: {
-							'data-xxx': 'xxx'
-						}
-					},
-					{
-						label: "12364",
+		Common.render(true, {
+			tpl: 'tpls/index.html',
+			data: '/resources/data/image.txt',
+			beforeRender: function(data) {
+				return $.extend({}, data, {
+					iSelectData: {
+						name: 'xxxx',
 						attrs: {
 							'data-xxx': 'xxx'
 						},
 						list: [
 							{
-								value: 56789,
+								value: 1245,
 								attrs: {
 									'data-xxx': 'xxx'
 								}
+							},
+							{
+								label: "12364",
+								attrs: {
+									'data-xxx': 'xxx'
+								},
+								list: [
+									{
+										value: 56789,
+										attrs: {
+											'data-xxx': 'xxx'
+										}
+									}
+								]
 							}
 						]
 					}
-				]
+				});
+			},
+			callback: function() {
+				Common.dataTable('#table-example', {
+					columnDefs: [
+						{
+							orderable: false,
+							targets: [0, 10]
+						}
+					]
+				});
 			}
-		}, function(data) {
-			Common.dataTable('#table-example', {
-				columnDefs: [
-					{
-						orderable: false,
-						targets: [0, 10]
-					}
-				]
-			});
 		});
 	};
 	return {
