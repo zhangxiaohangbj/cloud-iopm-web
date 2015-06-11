4d4df0e3f16b94a22e58137a57b9e52e9afa2be3
koulq <koulq@inspur.com>
2015-06-11 10:33:14 +0800

    配额

--- WebContent/resources/js/ccenter/vdc.js
+++ WebContent/resources/js/ccenter/vdc.js
@@ -34,6 +34,73 @@ define(['Common','bs/modal','bs/wizard','bs/tooltip','jq/form/validator','jq/for
 	    		$('.table-primary').find('input[type=checkbox]').iCheck('uncheck');
 	    	}
 	    });
+	    
+	    //更新配额
+	    $("ul.dropdown-menu a.updateQuota").on("click",function(){
+	    	require(['css!'+PubView.rqBaseUrl+'/css/dialog.css']);
+	    	EditData.EditVmName($(this).attr("data"));
+	    });
+    
+	    //更多
+	    var more = {
+		    	//配额管理
+		    	QuotaSets : function(id){
+		    		//先获取QuotaSets后，再render
+		    		Common.xhr.ajax('/v2.0/1/os-quota-sets/' + id,function(data){
+		    			Common.render('tpls/ccenter/vdc/quota.html',data,function(html){
+			    			Dialog.show({
+			    	            title: '子网创建',
+			    	            message: html,
+			    	            nl2br: false,
+			    	            buttons: [{
+			    	                label: '保存',
+			    	                action: function(dialog) {/*
+			    	                	var valid = $(".form-horizontal").valid();
+			    	            		if(!valid) return false;
+//			    	                	var serverData = {
+//			    	                			"id":"subnetid5",
+//			    	        					"name": $("#addSubnet [name='name']").val(),
+//			    	        					"cidr":  $("#addSubnet [name='cidr']").val(),
+//			    	        					"ip_version": $("#addSubnet [name='ip_version']").val(),
+//			    	        					"gateway_ip": $("#addSubnet [name='gateway_ip']").val(),
+//			    	        					"enable_dhcp":$("#addSubnet [name='enable_dhcp']:checked").length? 1:0
+//			    	        				};
+			    	                	var serverData = {
+			    	                		"subnet":{
+			    	                			"allocation_pools": [
+		           	    	                	      {
+		           	    	                	        "end": $("#addSubnet [name='end']").val()? $("#addSubnet [name='end']").val():254,
+		           	    	                	        "start": $("#addSubnet [name='start']").val()? $("#addSubnet [name='start']").val():1
+		           	    	                	      }
+		           	    	                	    ],
+		           	    	                	    "cidr":  $("#addSubnet [name='cidr']").val(),
+		           	    	                	    "enable_dhcp": $("#addSubnet [name='enable_dhcp']:checked").length? 1:0,
+		           	    	                	    "gateway_ip": $("#addSubnet [name='gateway_ip']").val(),
+		           	    	                	    "ip_version": $("#addSubnet [name='ip_version']").val(),
+		           	    	                	    "ipv6_address_mode": "",
+		           	    	                	    "ipv6_ra_mode": "",
+		           	    	                	    "name": $("#addSubnet [name='name']").val(),
+		           	    	                	    "network_id": $("#addSubnet [name='network_id']").val(),
+		           	    	                	    "shared": 0,
+		           	    	                	    "tenant_id": "vdcid1"
+			    	                		}
+			    	                	  };
+			    	                	Common.xhr.postJSON('/v2.0/subnets',serverData,function(data){
+			    	                		if(data){
+			    	                			alert("保存成功");
+			    	                			dialog.close();
+											}else{
+												alert("保存失败");
+											}
+										})
+			    	                */}
+			    	            }],
+			    	            onshown : function(){}
+			    	        });
+			    		});
+		    		})
+		    		
+		    	}
 	}	
 	return {
 		init : init
