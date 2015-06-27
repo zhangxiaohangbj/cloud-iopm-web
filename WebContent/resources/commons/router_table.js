5e077419b19c977c541db5bc9c12f525b84def08
jinzhk <jinzhk@inspur.com>
2015-06-27 15:56:13 +0800

    Merge branch 'master' of http://gitserver/iop/cloud-iopm-web
    Conflicts:
    	WebContent/resources/commons/router_table.js

--- WebContent/resources/commons/router_table.js
+++ WebContent/resources/commons/router_table.js
@@ -1,14 +1,6 @@
 /*!
  * 注册路由表
  */
-define({
-    '^#ccenter(!.*)?$': "js/ccenter/vm",
-    '^#monitor(!.*)?$': "js/monitor/task/strategyGroup",
-    '^#cresource(!.*)?$': "js/cresource/env",
-    '^#sysmanagement(!.*)?$': "js/sysmanagement/user",
-    '^#ccenter/vmtype/metadataList/(.*)$' :"js/ccenter/vmtype/metadata",
-    '^#ccenter/block/detail/(.*)$' :"js/ccenter/block/detail",
-    '^#ccenter/vdc/usage/(.*)$' :"js/ccenter/vdc/usage"
 define('commons/router_table', function() {
     return {
         '^#ccenter(!.*)?$': "js/ccenter/vm",
@@ -16,6 +8,7 @@ define('commons/router_table', function() {
         '^#cresource(!.*)?$': "js/cresource/env",
         '^#sysmanagement(!.*)?$': "js/sysmanagement/user",
         '^#ccenter/vmtype/metadataList/(.*)$' :"js/ccenter/vmtype/metadata",
-        '^#ccenter/block/detail/(.*)$' :"js/ccenter/block/detail"
+        '^#ccenter/block/detail/(.*)$' :"js/ccenter/block/detail",
+        '^#ccenter/vdc/usage/(.*)$' :"js/ccenter/vdc/usage"
     };
 });
