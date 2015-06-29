77afc15528567f4ce5402be93f4bc2e435e0f48f
koulq <koulq@inspur.com>
2015-06-29 10:33:06 +0800

    usage

--- WebContent/resources/js/ccenter/vdc/usage.js
+++ WebContent/resources/js/ccenter/vdc/usage.js
@@ -10,6 +10,7 @@ define(['Common','bs/modal','jq/form/wizard','bs/tooltip','jq/form/validator-bs3
 	    	start = decodeURIComponent(hashArr[hashArr.length-2]);
 	    	end = decodeURIComponent(hashArr[hashArr.length-1]);
 	    }
+	    debugger;
 	    var url = '/v2.0/'+Common.cookies.getVdcId()+'/os-simple-tenant-usage/' + vdc_id;
 	    if(start || end){
 	    	url = '/v2.0/'+Common.cookies.getVdcId()+'/os-simple-tenant-usage/' + vdc_id +'?start='+start+'&end='+end;
@@ -46,11 +47,18 @@ define(['Common','bs/modal','jq/form/wizard','bs/tooltip','jq/form/validator-bs3
 		 $("#submit_usage").on("click",function(){
 			 var time = $.trim($("#time_text").val());
 		     if(time == null || time == "")return;
-			 var hashArr = Common.hash;
-			 var start =  encodeURIComponent($.trim(time.split("-")[0]));
-		     var end = encodeURIComponent($.trim(time.split("-")[1]));
-		     //Common.router.route(Common.hash +'/' +start +'/'+end);
-		     window.location.href= Common.hash +'/' +start +'/'+end;
+			 var hashArr = Common.hash.split('/');
+			 if(hashArr.length == 6){
+				 var start =  encodeURIComponent($.trim(time.split("-")[0]));
+			     var end = encodeURIComponent($.trim(time.split("-")[1]));
+			     var vdc_id = hashArr[hashArr.length-3];
+			     window.location.href= "#ccenter/vdc/usage/" + vdc_id +'/' +start +'/'+end;
+			 }else{
+				 var start =  encodeURIComponent($.trim(time.split("-")[0]));
+			     var end = encodeURIComponent($.trim(time.split("-")[1]));
+			     window.location.href= Common.hash +'/' +start +'/'+end;
+			 }
+			
 		 })
 	}	
 	return {
