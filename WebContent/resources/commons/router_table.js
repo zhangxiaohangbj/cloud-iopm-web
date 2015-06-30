6720e6c24e92f8ad583889ccdcd4a981bafcb782
gaochuanji <gaochuanji@inspur.com>
2015-06-30 19:49:35 +0800

    初始云服务菜单

--- WebContent/resources/commons/router_table.js
+++ WebContent/resources/commons/router_table.js
@@ -3,7 +3,6 @@
  */
 define('commons/router_table', function() {
     return {
-        '^#ccenter(!.*)?$': "js/ccenter/vm",
         '^#monitor(!.*)?$': "js/monitor/task/strategyGroup",
         '^#cresource(!.*)?$': "js/cresource/env",
         '^#sysmanagement(!.*)?$': "js/sysmanagement/user",
