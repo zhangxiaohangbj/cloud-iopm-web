2595f32c254f736729eb5c2bdfdf65e24e3ab071
李永信 <inspur_lyx@hotmail.com>
2015-07-03 15:20:53 +0800

    菜单选中错误以及搜索样式

--- WebContent/resources/commons/main.js
+++ WebContent/resources/commons/main.js
@@ -1116,7 +1116,6 @@ define('commons/main',
                                     }
                                 },
                                 error: function(xhr, errorText) {
-                                    debugger;
                                 }
                             });
                         }
@@ -1125,7 +1124,6 @@ define('commons/main',
                         label: '取消',
                         cssClass: 'btn-primary',
                         action: function(dialog){
-                            debugger;
                             dialog.close();
                         }
                     }
