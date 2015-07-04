0dffb19584ec438496abb0bbf75ae64f5c15b186
gaochuanji <gaochuanji@inspur.com>
2015-07-04 15:18:24 +0800

    语法问题。

--- WebContent/resources/commons/router_table.js
+++ WebContent/resources/commons/router_table.js
@@ -4,9 +4,9 @@
 define('commons/router_table', function() {
     return {
         '^#monitor(!.*)?$': "js/monitor/task/strategyGroup",
-        '^#aservice(!.*)?$'' :"js/aservice/rds/instance",
-        '^#bservice(!.*)?$'' :"js/bservice/nosql/index",
-        '^#fservice(!.*)?$'' :"js/fservice/vm/index",
+        '^#aservice(!.*)?$' :"js/aservice/rds/instance",
+        '^#bservice(!.*)?$' :"js/bservice/nosql/index",
+        '^#fservice(!.*)?$' :"js/fservice/vm/index",
         '^#ccenter(!.*)?$' :"js/ccenter/region",
         '^#ccenter/zone/detail/(.*)?$': "js/ccenter/zone/detail",
         '^#ccenter/region/detail/(.*)?$': "js/ccenter/region/detail",
