a17ca60832077de5e08e4cbc8a7592794108e747
wang_gang <wang_gang@inspur.com>
2015-07-04 14:19:38 +0800

    autoscaling group detail page

--- WebContent/resources/js/fservice/autoscaling/detail.js
+++ WebContent/resources/js/fservice/autoscaling/detail.js
@@ -5,21 +5,16 @@ define(['Common','bs/modal','jq/form/wizard','bs/tooltip','jq/form/validator-bs3
 		var hashArr = Common.hash.split('/');
 	    var volume_id = hashArr[hashArr.length-1];
 		var tmpDetailData = {
-	            name : "Tiger Nixon",
-	            id: "di91jd9d29f9f29",
-	            size:"20GB",
-	            status: "in-use",
-	            vm_id: "private",
-	            type: "ceth(分布式)",
-	            vdc_id : "vdc1",
-	            available_zone: "WDK1",
-	            read_only:"1",
-	            description:"可用磁盘数据",
-	            mount_list: [{
-	            	name:'nexus-server',
-	            	id: 'nexus-server'
-	            }],
-	            mount_path: '/opt/vdc',
+	            name : volume_id,
+	        	"id" : "6",
+	            "conf":"test",
+	            "servers": "2",
+	            "require": "2",
+	            "min": "1",
+	            "max" : "5",
+	            "available_zone": "WDS-dec",
+	            "cool_time":"300",
+	            "status":"start",
 	            create_time: '2015-04-24 17:14:57'
     	};
     	Common.render(true,'tpls/fservice/autoscaling/detail.html',tmpDetailData,function(html){
@@ -55,95 +50,21 @@ define(['Common','bs/modal','jq/form/wizard','bs/tooltip','jq/form/validator-bs3
 					});
 				});
 			}else if('server' == data){
-				Common.render(false,'tpls/fservice/autoscaling/servers.html','/resources/data/volume.txt',function(html){
+				Common.render(false,'tpls/fservice/autoscaling/servers.html','/resources/data/servers.txt',function(html){
 					$tab.empty().append(html);
 				});
 			}else if('rule' == data){
-				Common.render(false,'tpls/fservice/autoscaling/rules.html','/resources/data/volume.txt',function(html){
+				Common.render(false,'tpls/fservice/autoscaling/rules.html','/resources/data/rules.txt',function(html){
 					$tab.empty().append(html);
 				});	
-			}else if('task' == data){
-				Common.render(false,'tpls/fservice/autoscaling/tasks.html','/resources/data/volume.txt',function(html){
+			}else if('activity' == data){
+				Common.render(false,'tpls/fservice/autoscaling/activities.html','/resources/data/activities.txt',function(html){
 					$tab.empty().append(html);
 				});					
-			}else if('event' == data){
-				require(['echarts','echarts/chart/line'],function (ec) {
-						Common.hideLocalLoading($tab);
-	                    //--- 折柱 ---
-	                    var myChart = ec.init($tab.find('.monitor-request-read')[0]);
-	                    var options = {
-	                    	    title : {
-	                    	        text: '读请求'
-	                    	    },
-	                    	    tooltip : {
-	                    	        trigger: 'axis'
-	                    	    },
-	                    	    legend: {
-	                    	        data:['最高请求','最低请求']
-	                    	    },
-	                    	    toolbox: {
-	                    	        show : true,
-	                    	        feature : {
-	                    	            mark : {show: true},
-	                    	            dataView : {show: true, readOnly: false},
-	                    	            magicType : {show: true, type: ['line', 'bar']},
-	                    	            restore : {show: true},
-	                    	            saveAsImage : {show: true}
-	                    	        }
-	                    	    },
-	                    	    calculable : true,
-	                    	    xAxis : [
-	                    	        {
-	                    	            type : 'category',
-	                    	            boundaryGap : false,
-	                    	            data : ['周一','周二','周三','周四','周五','周六','周日']
-	                    	        }
-	                    	    ],
-	                    	    yAxis : [
-	                    	        {
-	                    	            type : 'value',
-	                    	            axisLabel : {
-	                    	                formatter: '{value} 次'
-	                    	            }
-	                    	        }
-	                    	    ],
-	                    	    series : [
-	                    	        {
-	                    	            name:'最高',
-	                    	            type:'line',
-	                    	            data:[11, 11, 15, 13, 12, 13, 10],
-	                    	            markPoint : {
-	                    	                data : [
-	                    	                    {type : 'max', name: '最大值'},
-	                    	                    {type : 'min', name: '最小值'}
-	                    	                ]
-	                    	            },
-	                    	            markLine : {
-	                    	                data : [
-	                    	                    {type : 'average', name: '平均值'}
-	                    	                ]
-	                    	            }
-	                    	        },
-	                    	        {
-	                    	            name:'最低',
-	                    	            type:'line',
-	                    	            data:[1, -2, 2, 5, 3, 2, 0],
-	                    	            markPoint : {
-	                    	                data : [
-	                    	                    {name : '周最低', value : -2, xAxis: 1, yAxis: -1.5}
-	                    	                ]
-	                    	            },
-	                    	            markLine : {
-	                    	                data : [
-	                    	                    {type : 'average', name : '平均值'}
-	                    	                ]
-	                    	            }
-	                    	        }
-	                    	    ]
-	                    	};
-	                    myChart.setOption(options);
-	                }
-		        );
+			}else if('task' == data){
+				Common.render(false,'tpls/fservice/autoscaling/tasks.html','/resources/data/tasks.txt',function(html){
+					$tab.empty().append(html);
+				});	
 			}
 			Common.hideLocalLoading($tab);
 		}
